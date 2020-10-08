7590778782144b1c00173eaba7f076ae2d262e71
PrekshaMysore <53818119+PrekshaMysore@users.noreply.github.com>
2020-10-08 16:17:39 +0530

    Add files via upload

---
+++ lab3/lab3prog1.js
@@ -0,0 +1,12 @@
+db.createCollection("student1")
+db.student1.insert({_id:1,"name":"DFE","rollNo":"10","age":"20","contact":"99163 21232","emailId":"abc@gmail.com"});
+db.student1.insert({_id:2,"name":"ABC","rollNo":"11","age":"21","contact":"99163 89032","emailId":"defgh@gmail.com"});
+db.student1.insert({_id:3,"name":"GHI","rollNo":"12","age":"20","contact":"99243 21232","emailId":"abchi@gmail.com"});
+db.student1.insert({_id:4,"name":"JKL","rollNo":"13","age":"23","contact":"88163 21232","emailId":"jkl@gmail.com"});
+db.student1.insert({_id:5,"name":"MNO","rollNo":"14","age":"22","contact":"88967 21232","emailId":"mno@gmail.com"});
+db.student1.find({});
+db.student1.update({_id:1,"name":"DFE","rollNo":"10"}{$set:{"emailId":"dfe@gmail.com"}},{upsert:true});
+db.student1.replaceOne({_id:2,"rollNo":"11","name":"ABC"},{"name":"FEM"},{upsert:true});
+insert overwrite local directory 'C:\Users\Preksha\Desktop\mongodb'
+db.student1.find({});
+db.student1.drop();
---
+++ lab3/lab3prog2.js
@@ -0,0 +1,14 @@
+db.createCollection("Customers")
+db.Customers.insert({_id:1,"name":"abcd","Cust_Id":"100","Acc_Bal":"22000","Acc_Type":"X"});
+db.Customers.insert({_id:2,"name":"efgh","Cust_Id":"121","Acc_Bal":"212002","Acc_Type":"Z"});
+db.Customers.insert({_id:3,"name":"ijk","Cust_Id":"130","Acc_Bal":"400000","Acc_Type":"Z"});
+db.Customers.insert({_id:4,"name":"lmno","Cust_Id":"100","Acc_Bal":"23000","Acc_Type":"Y"});
+db.Customers.insert({_id:5,"name":"pqr","Cust_Id":"130","Acc_Bal":"6000","Acc_Type":"X"});
+db.Customers.find({});
+db.Customers.find({"Acc_Bal":{"$gte":"1200"}},{"Acc_Type":"Z",_id:0});
+db.Customers.find({"Acc_Bal":{"$min":"","$max":""}},{"Cust_Id":"100",_id:0});
+db.Customers.find({"Acc_Bal":{"$min":"","$max":""}},{"Cust_Id":"121",_id:0});
+db.Customers.find({"Acc_Bal":{"$min":"","$max":""}},{"Cust_Id":"130",_id:0});
+insert overwrite local directory ({'C:\Users\Preksha\Desktop\mongodb'})
+db.Customers.find({});
+db.Customers.drop();
